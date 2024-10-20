6c47460caa00179ca34c4cfea585fb5414046192
dev <34762603+dev2298@users.noreply.github.com>
2024-10-20 22:20:29 +0530

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -49,7 +49,7 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href="#testimonials" className="page-scroll">
-                Testimonials
+                ClimateEffect
               </a>
             </li>
             <li>
